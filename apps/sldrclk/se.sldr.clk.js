b7b4109b582fbe7918e00d3611bff1cc8bfb8a69
Jonas A. Hultén <jonas@sldr.se>
2023-08-06 23:48:44 +0200

    feat(sldrclk): date information line

--- apps/sldrclk/se.sldr.clk.js
+++ apps/sldrclk/se.sldr.clk.js
@@ -21,16 +21,14 @@ const fastClockPos = [slowClockPos[0] + 70, dmax / 2]; // element is 14x11
 const datePos = [(dmax - 50) / 2, slowClockPos[1] - padding - 7]; // element is 50x7
 const timestampPos = [datePos[0], slowClockPos[1] + 22 + padding]; // element is 50x7
 const tzPos = [(dmax - 40) / 2, (dmax - 7 - padding)]; // element is 40x8
+const dateLinePos = [49, tzPos[1] - padding - 8]; // element is 78x8
 
-console.log([slowClockPos, fastClockPos, datePos, timestampPos, tzPos]);
+console.log([slowClockPos, fastClockPos, datePos, timestampPos, tzPos, dateLinePos]);
 
 // Create minute ticker
 var minute = 0;
 
 // TODO
-// Weekday
-// Days in Month
-// Week number
 // Percent of Day
 // Percent of Year
 // Moon cycle
@@ -98,6 +96,52 @@ function drawTZ(d) {
   g.drawString(time, tzPos[0], tzPos[1], true);
 }
 
+function calcWeekNo(d) {
+  // Define a new date that's in UTC
+  var dUTC = new Date(d - (d.getTimezoneOffset() * 60 * 1000));
+  // Redefine that date again, but at midnight
+  var dMidnight = new Date(dUTC.getFullYear(), dUTC.getMonth(), dUTC.getDate());
+  // Set the day to the nearest Thursday (current date + 4 - current day number)
+  dMidnight.setDate(dMidnight.getDate() + 4 - (dMidnight.getDay() || 7));
+  // Define the start of the year
+  var dYearStart = new Date(dMidnight.getFullYear(), 0, 1);
+  var weekNo = Math.ceil((((dMidnight - dYearStart) / 86400000) + 1) / 7);
+  return weekNo;
+}
+
+function drawDateLine(d) {
+  // Time math
+  var day = d.toString().split(" ")[0].toUpperCase();
+  var dom = new Date(d.getFullYear(), d.getMonth() + 1, 0).getDate();
+  var week = calcWeekNo(d).toString().padStart(2, 0);
+  // Reset the graphics
+  g.reset();
+  // Draw the day header (8x6)
+  g.setFont(extraOtherFont);
+  var x = dateLinePos[0], y = dateLinePos[1] + 2;
+  g.drawString("D:", x, y, true);
+  // Draw the day (18x8)
+  x += 8, y -= 2;
+  g.setFont(mainOtherFont);
+  g.drawString(day, x, y, true);
+  // Draw the days in month header (12x6)
+  x += (padding + 18), y += 2;
+  g.setFont(extraOtherFont);
+  g.drawString("MD:", x, y, true);
+  // Draw the days in month (12x8)
+  x += 12, y -= 2;
+  g.setFont(mainOtherFont);
+  g.drawString(dom, x, y, true);
+  // Draw the week header (8x6)
+  x += (padding + 12), y += 2;
+  g.setFont(extraOtherFont);
+  g.drawString("W:", x, y, true);
+  // Draw the week number (12x8)
+  x += 8, y -= 2;
+  g.setFont(mainOtherFont);
+  g.drawString(week, x, y, true);
+}
+
 /* Battery economy block */
 
 function drawFast(d) {
@@ -109,6 +153,7 @@ function drawSlow(d) {
   drawSlowClock(d);
   drawDate(d);
   drawTZ(d);
+  drawDateLine(d);
 }
 
 function drawLoop() {
@@ -174,19 +219,6 @@ drawAll();
 
 // function getUTCTime(d) {
 //   return d.toUTCString().split(' ')[4].split(':').map(function (d) { return Number(d) });
-// }
-
-// function getWeekNumber(d) {
-//   // Define a new date that's in UTC
-//   d = new Date(d - (d.getTimezoneOffset() * 60 * 1000));
-//   // Redefine that date again, but at midnight
-//   d = new Date(d.getFullYear(), d.getMonth(), d.getDate());
-//   // Set the day to the nearest Thursday (current date + 4 - current day number)
-//   d.setDate(d.getDate() + 4 - (d.getDay() || 7));
-//   // Define the start of the year
-//   var yearStart = new Date(d.getFullYear(), 0, 1);
-//   var weekNo = Math.ceil((((d - yearStart) / 86400000) + 1) / 7);
-//   return weekNo;
 // }
 
 // function getMoonPhase(d) {
@@ -256,13 +288,6 @@ drawAll();
 //   var d = new Date();
 //   var da = d.toString().split(" ");
 //   var dutc = getUTCTime(d);
-
-//   //Days in month
-//   var dom = new Date(d.getFullYear(), d.getMonth() + 1, 0).getDate();
-
-//   // Draw day of week, days in month, and week number
-//   g.setFont(font, smallFontSize);
-//   g.drawString(`d:${locale.dow(d, true)} md:${dom} w:${getWeekNumber(d)}`, xyCenter, yposDml, true);
 
 //   // Draw percent passed of day and year
 //   g.setFont(font, smallFontSize);
